fe94e0758f0212a1fa7fa761bd77eac1fed14ff3
Josh Ruud <joshruud@Joshs-MacBook-Pro.local>
2018-07-05 19:10:39 -0400

    Added server and config files

---
+++ src/server.js
@@ -0,0 +1,20 @@
+// src/server.js
+
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const config = require('./config');
+const router = require('./routes');
+
+// Load mongoose package
+const mongoose = require('mongoose');
+
+const app = express();
+const publicPath = path.resolve(__dirname, '../public');
+app.use(bodyParser.json());
+app.use(express.static(publicPath));
+app.use('/api', router);
+
+app.listen(config.port, function () {
+  console.log(`${config.appName} is listening on port ${config.port}`);
+})
